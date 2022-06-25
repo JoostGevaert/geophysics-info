5d62c62994053edb4049070d37fc11bdf384a7e5
Joost Gevaert <Joost.Gevaert@arup.com>
2022-06-25 17:35:34 +0200

    feat: add posts and authors pages

---
+++ pages/authors/joop-gevaar.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function JoopGevaar() {
+  return (
+    <>
+      <h1>Author: Joop Gevaar ⚠</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
