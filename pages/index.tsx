a68233f1ef1c2de731e88fb9467fb5de0531553b
Joost Gevaert <Joost.Gevaert@arup.com>
2022-07-04 10:11:08 +0200

    feat: Chakra Container, Flex and VStack Components

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,34 @@ import type { NextPage } from 'next'
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import { Container, Flex, VStack } from '@chakra-ui/react';
 import Layout, { siteTitle } from '../components/layout';
 import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts'
+
+const IndexPage = () => {
+  return (
+    <Container maxW="container.xl" p={0}>
+      <Flex h="100vh" py={20}>
+        <VStack 
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+        ></VStack>
+        <VStack 
+          w="full"
+          h="full"
+          p={10}
+          spacing={10}
+          alignItems="flex-start"
+          bg="gray.50"
+        ></VStack>
+      </Flex>
+    </Container>
+  )
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
@@ -15,7 +40,7 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-export default function Home ({
+const Home = ({
   allPostsData
 }: {
   allPostsData: {
@@ -23,7 +48,7 @@ export default function Home ({
     title: string
     id: string
   }[]
-}) {
+}) => {
   return (
     <Layout home>
       <Head>
@@ -55,3 +80,5 @@ export default function Home ({
     </Layout>
   )
 }
+
+export default IndexPage;
