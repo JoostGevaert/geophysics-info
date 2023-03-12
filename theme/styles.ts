ed9fbfda7c29cbdc224058ece22b97bdf5208f81
JoostGevaert <jhngevaert@gmail.com>
2023-03-12 16:10:21 +0100

    Initial commit after astro.build setup

--- theme/styles.ts
+++
@@ -1,9 +0,0 @@
-const styles = {
-  fonts: {
-		body: "Segoe UI, system-ui, sans-serif",
-		heading: "Segoe UI, system-ui, sans-serif",
-		mono: "Consolas, monospace",
-	},
-}
-
-export default styles;
