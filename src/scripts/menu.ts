76b3afa42b0a29401b6535f522302c3b9186d8eb
JoostGevaert <jhngevaert@gmail.com>
2023-03-16 21:58:59 +0100

    Create Hamburger navigation menu for small screens

---
+++ src/scripts/menu.ts
@@ -0,0 +1,16 @@
+// TypeScript code
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+if (hamburger && navLinks) {
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("expanded");
+  });
+}
+
+export {} // Bit weird that this empty export is needed?
+
+// Javascript code
+// document.querySelector('.hamburger').addEventListener('click', () => {
+//     document.querySelector('.nav-links').classList.toggle('expanded');
+//   });
